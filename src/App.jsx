f64536c37fdbaf2bd39838f5921df7d7e834d348
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2024-11-19 20:52:32 -0500

    bunch of things

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import { useState } from 'react';
 import './style/App.scss';
 import { Helmet } from 'react-helmet-async';
 import { AppWrapper, Window } from './sdk/sdk';
+import { generateId } from "./sdk/modules/Lib";
+import { useHotkeys } from 'react-hotkeys-hook';
 const App=({})=>{
+  const[debugMenuVisibility,setDebugMenuVisibility]=useState(false);
+  useHotkeys('ctrl+`',(e)=>{
+    e.preventDefault();
+    setDebugMenuVisibility(!debugMenuVisibility);
+  });
   return(<>
     <Helmet>
       <title>Beansite 7</title>
@@ -12,7 +19,8 @@ const App=({})=>{
       <Window
         data={{
           title:"Test",
-          icon:"",
+          icon:"/icons/15.ico",
+          id:generateId(10),
           includeTitlebarButtons:["close","min","max"],
         }}>
           <h1>Hello, World!</h1>
--- src/sdk/modules/Windows.jsx
+++ src/sdk/modules/Windows.jsx
@@ -1,45 +1,72 @@
 import "../style/Window.scss";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import { motion, useDragControls, useMotionValue } from "motion/react";
+import { generateId } from "./Lib";
 export const Window=({
   dragConstraint,
   children,
+  closed=false,
+  draggable=true,
   data:{
     title,
     icon,
     id,
-    includeTitlebarButtons,
-    x=0,
-    y=0,
-    height=280,
-    width=360,
+    includeTitlebarButtons=[],
+    x=15,
+    y=15,
+    height=225,
+    width=300,
   }
 })=>{
-  const [isDraggingY,setIsDraggingY]=useState(false);
+  const[isDraggingY,setIsDraggingY]=useState(false);
   const mHeight=useMotionValue(height);
-  const handleDragY=useCallback((event, info)=>{
+  const handleDragY=useCallback((e,info)=>{
     let newHeight=mHeight.get()+info.delta.y;
     if(newHeight>150&&newHeight<2000)mHeight.set(mHeight.get()+info.delta.y);
   },[]);
-  const [isDraggingX,setIsDraggingX]=useState(false);
+  const[isDraggingX,setIsDraggingX]=useState(false);
   const mWidth=useMotionValue(width);
-  const handleDragX=useCallback((event, info)=>{
+  const handleDragX=useCallback((e,info)=>{
     let newWidth=mWidth.get()+info.delta.x;
     if(newWidth>150&&newWidth<2000)mWidth.set(mWidth.get()+info.delta.x);
   },[]);
+  const[ani,setAni]=useState(closed?{opacity:0,y:5}:{opacity:1,y:0});
   return(<>
     <motion.div 
       drag
       dragConstraints={dragConstraint}
+      dragListener={!(isDraggingX||isDraggingY)&&draggable}
       dragMomentum={false}
+      transition={{duration:.35,delay:.5}}
       initial={{opacity:0,y:5}}
-      whileInView={{opacity:1,y:0}}
-      transition={{duration:.15,delay:.15}}
+      animate={ani}
       className="window" 
       id={id} 
-      style={{height:mHeight,width:mWidth}}>
-        <div className="tb" id={`${id}tb`}>
+      style={{height:mHeight,width:mWidth,top:y,left:x}}>
+        <div className="tb" id={`${id}_tb`}>
+          <div 
+            className="ico" id={`${id}_ico`}
+            style={{
+              backgroundImage:icon?`url('${icon}')`:"",
+            }}></div>
           <h1>{title}</h1>
+          <div className="bw" id={`${id}_bw`}>
+            {includeTitlebarButtons.map((data,index)=><>
+              {data=="close"?
+                <button 
+                  key={`${generateId(10)}_${btoa(index)}`} 
+                  onPointerDownCapture={e=>e.stopPropagation()}
+                  className="close"
+                  onClick={(e)=>{
+                    e.preventDefault();
+                    setAni({opacity:0,y:5});
+                    setTimeout(
+                      ()=>{document.getElementById(`${id}_tb`).style.display="none"},
+                      1000);
+                  }}>x</button>
+                :null}
+            </>)}
+          </div>
         </div>
         <div 
           className="contents" 
@@ -53,10 +80,10 @@ export const Window=({
           dragElastic={0}
           dragMomentum={false}
           onDrag={handleDragY}
-          onDragEnd={() => {
+          onDragEnd={(e)=>{
             setIsDraggingY(false);
           }}
-          onDragStart={() => {
+          onDragStart={(e)=>{
             setIsDraggingY(true);
           }}></motion.div>
         <motion.div
@@ -66,10 +93,10 @@ export const Window=({
           dragElastic={0}
           dragMomentum={false}
           onDrag={handleDragX}
-          onDragEnd={() => {
+          onDragEnd={(e)=>{
             setIsDraggingX(false);
           }}
-          onDragStart={() => {
+          onDragStart={(e)=>{
             setIsDraggingX(true);
           }}></motion.div>
     </motion.div>
